42e82c1aeea1e3510ffbce7e13443fbf04c98c11
JHenrique <henryque.pc.game@gmail.com>
2026-03-25 17:47:19 -0300

    prisma: paginaçao take e skip

--- src/app/page.js
+++ src/app/page.js
@@ -34,13 +34,24 @@ async function getAllPosts(page = 1) {
   // return response.json();
 
   try {
+    const perPage = 6;
+    const skip = (page - 1) * perPage;
+
+    const prev = page > 1 ? page - 1 : null;
+    const totalPosts = await db.post.count();
+    const totalPages = Math.ceil(totalPosts / perPage);
+    const next = page < totalPages ? page + 1 : null;
+
     const posts = await db.post.findMany({
-      // include: {
-      //   author: true,
-      // },
+      take: perPage,
+      orderBy: { createdAt: "desc" },
+      skip: skip,
+      include: {
+        author: true,
+      },
     });
 
-    return { data: posts, prev: null, next: null };
+    return { data: posts, prev, next };
   } catch (error) {
     logger.error("Falha ao obter posts", { error });
     return { data: [], prev: null, next: null };
