2c1eda4d6464d3a892c37240afd25d5f580256d2
JHenrique <henryque.pc.game@gmail.com>
2026-03-27 18:03:19 -0300

    erro deploy vercal 10

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,10 +4,10 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  // migrations: {
-  //   path: "prisma/migrations",
-  //   //seed: "tsx prisma/seed.js",
-  // },
+  migrations: {
+    path: "prisma/migrations",
+    seed: "tsx prisma/seed.js",
+  },
   datasource: {
     url: env("PRISMA_DATABASE_URL"),
   },
